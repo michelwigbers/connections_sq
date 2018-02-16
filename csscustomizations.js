8441c9569adfddacc283bacc267acd30799534bc
Jovanny Martis <jmartis@users.noreply.github.com>
2018-02-16 12:25:55 +0100

    Update csscustomizations.js

--- csscustomizations.js
+++ csscustomizations.js
@@ -1,7 +1,7 @@
 console.log('connectons_sq loaded');
 if(typeof(dojo) != "undefined") {
     dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/connections_sq/customization.css?repoName=connections_sq\"></link>",
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/customization.css?repoName=connections_sq\"></link>",
         dojo.doc.head,
         "last"
     );
